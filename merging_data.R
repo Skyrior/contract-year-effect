425a19406a86ab4dba0e5ab4663828ec7ff63446
jonathanliuuchicago <66703891+jonathanliuuchicago@users.noreply.github.com>
2020-06-11 17:35:08 -0500

    Create merging_data.R

---
+++ merging_data.R
@@ -0,0 +1,19 @@
+boxouts$Season <- boxouts$Season+1
+touches$Season <- touches$Season + 1
+
+nbaadvancedstats$Season <- nbaadvancedstats$season
+
+boxouts$name <- boxouts$Player
+touches$name <- touches$Player
+
+boxouts$Season <- as.numeric(boxouts$Season)
+nbaadvancedstats$Season <- as.numeric(nbaadvancedstats$Season)
+touches$Season <- as.numeric(touches$Season)
+nbaadvancedstats$name <- gsub("_", " ", nbaadvancedstats$name)
+
+merged_nba <- merge(nbaadvancedstats, boxouts, by=c("name", "Season"))
+merged_nba <- merge(merged_nba, touches, by=c("name", "Season"))
+
+library(csv)
+library(readr)
+readr::write_csv(merged_nba, "merged_nba.csv")
