1bbbfd733e9c0731a885e25b6cfec71b354267a9
trg1234 <46304393+trg1234@users.noreply.github.com>
2020-06-12 00:02:55 -0500

    s

--- seandata.R
+++ seandata.R
@@ -1,4 +1,24 @@
 rm(list=ls())
+library(plyr)
+library(ggplot2)
+
+
 data <- read.csv(file = 'nba.csv')
 
 View(data)
+
+
+
+ss.pos <- subset(data, select = c('pos'))
+
+table(ss.pos)
+
+
+reorder_size <- function(x) {
+  factor(x, levels = names(sort(table(x), decreasing = TRUE)))
+}
+
+position <- ggplot(ss.pos, aes(x=reorder_size(pos))) + geom_bar() + xlab("Position")
+
+position
+
